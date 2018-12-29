af6a18b2f0bc31d212d5551463e4cbe6ea116151
raghuvararora <raghuvararora@gmail.com>
2018-12-29 17:42:19 +0530

    added few routes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var mysql      = require('mysql');
 var express    =require("express");
+var bodyParser =require('body-parser');
+
+
 
 var app = express();
 var connection = mysql.createConnection({
@@ -10,6 +13,9 @@ var connection = mysql.createConnection({
     socketPath: '/var/run/mysqld/mysqld.sock'
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // connection.connect((err)=> {
 //     if (err) {
 //       console.error('error connecting: ' + err.stack);
@@ -19,8 +25,35 @@ var connection = mysql.createConnection({
 //     console.log('connected as id ' + connection.threadId);
 //   });
 
+app.get("/company/new", (req, res)=>{
+    res.send("");
+});
+app.post("/company/new",(req, res)=>{
+    connection.query("insert into company_master set ? ",  req.body,(error, result, fields)=>{
+        if(error){
+            console.log("try again")
+        }
+    } )
+    
+} );
+app.get("/company/:id/edit", (req, res)=>{
+    connection.query("select * from company_master where id=?",req.params.id, (error, result, fields)=>{
+        if(error){
+            //render /comapny/new template with  existing data
+            console.log("error in  retrieving");
+        }
+    } )
+});
+app.put("/company/:id/edit", (req, res)=>{
+    connection.query("update company_master set ? where id=? ", [req.body, req.params.id], (error, result, fields)=>{
+        if(error){
+            console.log("try again later");
+        }
+    })
+});
+app.post("/item/new")
+app.put("/item/:id")
 
-app.post("/")
 
 connection.query("show databases;", (error, result,fields)=>{
       console.log(result[1].Database);
