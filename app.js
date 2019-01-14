65aeb9b6916cbcf5b9a22aabedde5af0a1c03726
raghuvararora <raghuvararora@gmail.com>
2019-01-14 05:37:15 +0530

    improved visually a little

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const mysql = require('mysql');
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const mySQLConfig = require('./config.js');
-
+const mysql = require( "mysql" );
+const express = require( "express" );
+const bodyParser = require( "body-parser" );
+const ejs = require( "ejs" );
+const mySQLConfig = require( "./config.js" );
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.set('view engine', 'ejs');
+app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use( bodyParser.json() );
+app.set( "view engine", "ejs" );
 
+app.use( "/", require( "./routes/company.js" ) );
 
-app.use('/', require('./routes/company.js'));
-
-app.listen(8008, () => { console.log('hello'); });
+app.listen( 8008, () => { console.log( "hello" ); } );
--- routes/company.js
+++ routes/company.js
@@ -1,58 +1,42 @@
-let mysql      =require('mysql');
-let express    =require("express");
-let router     =express.Router();
-let mySQLConfig=require("../config.js");
-let knex       =require("../knex.js");
+const mysql = require( "mysql" );
+const express = require( "express" );
 
+const mySQLConfig = require( "../config.js" );
+const knex = require( "../knex.js" );
 
-let connection = mysql.createConnection(mySQLConfig);
+const router = express.Router();
 
-connection.connect((err)=> {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('connected as id ' + connection.threadId);
-  });
+const connection = mysql.createConnection( mySQLConfig );
 
-connection.query("use magnaaccounts;", (error, result,fields)=>{
-      console.log(result);
-      if(error){
-          console.log(error.code);
-      }
-  })
+router.get( "/company/new", ( req, res ) => {
+    // code to render newCompany form
+    console.log( "yoy" );
+    res.render( "newCompany.ejs" );
+    connection.query( "show databases", ( error, result, fields ) => {
+        console.log( result );
+    } );
+} );
+router.post( "/company/new", ( req, res ) => {
+    console.log( "right here sir", req.body );
+    const data = req.body;
+    console.log( data.name );
 
-
-router.get("/company/new", (req, res)=>{
-    //code to render newCompany form 
-    console.log("yoy")
-    res.render("newCompany.ejs");
-    connection.query("show databases", (error, result, fields)=>{
-        console.log(result);
-    })
-});
-router.post("/company/new",(req, res)=>{
-    console.log("right here sir", req.body);
-    let data=req.body;
-    console.log(data.name);
-
-    knex("company_master")
-        .insert({
-            name:data.name,
-            address:data.address,
-            email:data.email,
-            contact:data.contact,
-            gst:data.gst
-        })
-        .returning("id")
-        .then((id)=>{
-            console.log(id, "right here" );
-            res.redirect(201, "/company/"+id)
-        })
-        .catch((error)=>{
-            console.log("error inserting",error);
-
-        })
+    knex( "company_master" )
+        .insert( {
+            name: data.name,
+            address: data.address,
+            email: data.email,
+            contact: data.contact,
+            gst: data.gst,
+        } )
+        .returning( "id" )
+        .then( ( id ) => {
+            console.log( id, "right here" );
+            res.redirect( 201, "/company/" + id );
+        } )
+        .catch( ( error ) => {
+            console.log( "error inserting", error );
+        } );
     // connection.query("insert into company_master(name, address, email, contact, gst) values(?,?,?,?,?); ", [data.name, data.address,data.email,data.contact, data.gst] ,(error, result, fields)=>{
     //     if(error){
     //         console.log(error);
@@ -67,49 +51,45 @@ router.post("/company/new",(req, res)=>{
     //         res.redirect(201, "/company/"+result.insertId)
     //     }
     // } )
-    
 } );
 
-router.get("/company/:id", (req,res)=>{
-    
-    knex("company_master")
-        .where({id: req.params.id})
-        .then(row=>{
-            console.log(row);
-            res.render("listCompany.ejs",{row:row[0]});
-        })
-        .catch(error=>{
-            console.log(error);
-            res.send()
-        })
-});
+router.get( "/company/:id", ( req, res ) => {
+    knex( "company_master" )
+        .where( { id: req.params.id } )
+        .then( ( row ) => {
+            console.log( row );
+            res.render( "listCompany.ejs", { row: row[ 0 ] } );
+        } )
+        .catch( ( error ) => {
+            console.log( error );
+            res.send();
+        } );
+} );
 
-
-router.get("/company/:id/edit", (req, res)=>{
-    connection.query(" select * from company_master where id=?;",parseInt(req.params.id), (error, result, fields)=>{
-        if(error){
-            //render /comapny/new template with  existing data
-            console.log("error in  retrieving",error);
+router.get( "/company/:id/edit", ( req, res ) => {
+    connection.query( " select * from company_master where id=?;", parseInt( req.params.id ), ( error, result, fields ) => {
+        if ( error ) {
+            // render /comapny/new template with  existing data
+            console.log( "error in  retrieving", error );
+        } else {
+            res.render( "editCompany.ejs", result[ 0 ] );
         }
-        else{
-            res.render("editCompany.ejs", result[0] );
+    } );
+} );
+router.put( "/company/:id/edit", ( req, res ) => {
+    connection.query( "update company_master set ? where id=? ", [ req.body, req.params.id ], ( error, result, fields ) => {
+        if ( error ) {
+            console.log( "try again later" );
         }
-    } )
-});
-router.put("/company/:id/edit", (req, res)=>{
-    connection.query("update company_master set ? where id=? ", [req.body, req.params.id], (error, result, fields)=>{
-        if(error){
-            console.log("try again later");
-        }
-    })
-});
+    } );
+} );
 
-router.get("/company", (req,res)=>{
-    knex('company_master').
-        then(rows=>{
-            console.log("right here,", rows[0]);
-            res.render("getCompany.ejs",{rows:rows});
-        })
-} )
+router.get( "/company", ( req, res ) => {
+    knex( "company_master" )
+        .then( ( rows ) => {
+            console.log( "right here,", rows[ 0 ] );
+            res.render( "getCompany.ejs", { rows } );
+        } );
+} );
 
-module.exports=router;
+module.exports = router;
